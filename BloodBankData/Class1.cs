4b05df48a194653d8df107b6af182f964328b3d6
Frank Carag <cfrankluis@gmail.com>
2021-10-23 10:18:50 -0400

    Add project files.

---
+++ BloodBankData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankData
+{
+    public class Class1
+    {
+    }
+}
